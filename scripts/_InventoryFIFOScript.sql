2f1207f8d60aabafcfe7900ac5b67b57da148c6b
Andrii Yustyk <a.yustyk+publicgit@gmail.com>
2024-03-21 02:28:27 +0200

    Cleanup previous fifo scripts

--- scripts/_InventoryFIFOScript.sql
+++
@@ -1,8 +0,0 @@
-﻿DECLARE @UnitID int = 1;
-DECLARE @CountDate date = '2023-12-08';
--- SET @UnitID = 5;
--- SET @CountDate = '2024-01-01';
-
-/* For dbo.GetFiFo(...) please check _InventoryFunctions.sql */
-SELECT DISTINCT ItemID AS ItemID, CAST(dbo.GetFiFo(@UnitID, @CountDate, ItemID) as decimal(38, 2)) AS FIFO
-    FROM dbo.ItemsToCalcCost;
